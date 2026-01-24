7a6bd70ca3c612f6408b2b1eafc8af58fe36cef5
GitAvatq <ufigma2@gmail.com>
2026-01-25 00:27:05 +0600

    style:edited ui in profile page

--- src/appPages/site/components/pages/profileSections/Profile.tsx
+++ src/appPages/site/components/pages/profileSections/Profile.tsx
@@ -9,9 +9,13 @@ import {
 	IconPhone,
 	IconPhoto
 } from '@tabler/icons-react';
+import { MdModeEdit } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 import { FaTelegram } from 'react-icons/fa';
-import { ImExit } from 'react-icons/im';
+import { IoPersonCircleOutline } from "react-icons/io5"; import { BsTwitterX } from "react-icons/bs";
 import { Button, Modal, Skeleton, TextInput } from '@mantine/core';
+import { FaInstagram } from "react-icons/fa6";
 import { useGetMeQuery, useUpdateMeMutation } from '@/redux/api/me';
 import { useUploadFileMutation } from '@/redux/api/upload';
 import {
@@ -118,7 +122,7 @@ const Profile: FC = () => {
 		if (imageFile.type !== 'image/gif') {
 			fileToUpload = await imageCompression(imageFile, options);
 		}
-		const formData = new FormData();
+		const formData = new FormData(); // formData is useful for key-value data and allows to send these data to backend
 		formData.append('file', fileToUpload);
 		try {
 			const { data } = await uploadFileMutation(formData);
@@ -167,92 +171,114 @@ const Profile: FC = () => {
 		<>
 			<section className={scss.Profile}>
 				<div className="container">
+					<h1 className={scss.left__title}>
+						Профиль
+					</h1>
 					<div className={scss.content}>
-						<div className={scss.top}>
-							<h1 className={scss.title}>
-								Your <span>profile</span>
-							</h1>
-							<div className={scss.backgroundImage_panel}>
-								<Skeleton
-									visible={isLoadingGetMe}
-									className={scss.backgroundImage_skeleton}
-								>
-									{!isLoadingGetMe && (
-										<img
-											className={scss.backgroundImage}
-											src={selectedBackgroundImage || '/bg-default.png'}
-											alt={tempFirstName}
-											onLoad={() => setIsLoadingBackground(false)}
-										/>
-									)}
-								</Skeleton>
-								<Button
-									loading={isLoadingBackground}
-									loaderProps={{ type: 'dots' }}
-									leftSection={<IconPhoto size={18} />}
-									size="xs"
-									onClick={handleEditBackgroundClick}
-								>
-									Edit Background Image
-								</Button>
-								<input
-									ref={backgroundInputRef}
-									type="file"
-									style={{ display: 'none' }}
-									onChange={handleBackgroundChange}
-								/>
+						<div className={scss.left}>
+							<div className={scss.cover}>
+
+								<div className={scss.backgroundImage_panel}>
+									<Skeleton
+										visible={isLoadingGetMe}
+										className={scss.skeleton}
+									>
+										{!isLoadingGetMe && (
+											<img
+												className={scss.backgroundImage}
+												src={selectedBackgroundImage || '/bg-default.png'}
+												alt={tempFirstName}
+												onLoad={() => setIsLoadingBackground(false)}
+											/>
+										)}
+									</Skeleton>
+									<Button
+										loading={isLoadingBackground}
+										loaderProps={{ type: 'dots' }}
+										size="xs"
+										className={scss.bg_btn}
+										onClick={handleEditBackgroundClick}
+									>
+										<MdModeEdit size={40} />
+									</Button>
+									<input
+										ref={backgroundInputRef}
+										type="file"
+										style={{ display: 'none' }}
+										onChange={handleBackgroundChange}
+									/>
+								</div>
+								{/*  Avatar , upload block goes here */}
+								<div className={scss.left__avatar}>
+									<Skeleton
+										visible={isLoadingGetMe}
+										className={scss.avatar_skeleton}
+									>
+										{!isLoadingGetMe && (
+											<img
+												className={scss.avatar}
+												src={selectedPhoto || undefined}
+												alt={tempFirstName}
+												onLoad={() => setIsLoadingAvatar(false)}
+											/>
+										)}
+									</Skeleton>
+									<div className={scss.text}>
+										<span>Ваше фото</span>
+										<p>Здест будет отображатся ваше фото</p>
+									</div>
+								</div>
+								<div className={scss.left_bottom}>
+									<Button
+										className={scss.avatar_btn}
+										loading={isLoadingAvatar}
+										loaderProps={{ type: 'dots', color: "dark" }}
+										leftSection={<IconCamera size={18} />}
+										size="xs"
+										onClick={handleEditAvatarClick}
+									>
+										Загрузить фото
+									</Button>
+									<input
+										ref={avatarInputRef}
+										type="file"
+										style={{ display: 'none' }}
+										onChange={handleAvatarChange}
+									/>
+								</div>
 							</div>
-						</div>
-						<div className={scss.bottom}>
-							<div className={scss.left}>
-								<Skeleton
-									visible={isLoadingGetMe}
-									className={scss.avatar_skeleton}
-								>
-									{!isLoadingGetMe && (
-										<img
-											className={scss.avatar}
-											src={selectedPhoto || undefined}
-											alt={tempFirstName}
-											onLoad={() => setIsLoadingAvatar(false)}
-										/>
-									)}
-								</Skeleton>
-								<Button
-									loading={isLoadingAvatar}
-									loaderProps={{ type: 'dots' }}
-									leftSection={<IconCamera size={18} />}
-									size="xs"
-									onClick={handleEditAvatarClick}
-								>
-									Edit Avatar
-								</Button>
-								<input
-									ref={avatarInputRef}
-									type="file"
-									style={{ display: 'none' }}
-									onChange={handleAvatarChange}
-								/>
-							</div>
-							<div className={scss.right}>
-								<div className={scss.right_left}>
-									<div className={scss.user_name_block}>
+
+
+							{/* Personal informations , edit them goes here */}
+							<div className={scss.left__personal_info}>
+								<span className={scss.title}>Персональная информация</span>
+
+								<div className={scss.info_block}>
+									<div className={scss.name}>
 										<TextInput
+											leftSection={< IoPersonCircleOutline size={20} />}
 											label="FirstName"
 											value={tempFirstName}
 											onChange={handleFirstNameChange}
 										/>
+									</div>
+									<div className={scss.lastname}>
 										<TextInput
+											leftSection={< IoPersonCircleOutline size={20} />}
 											label="LastName"
 											value={tempLastName}
 											onChange={handleLastNameChange}
 										/>
-										{isUpdating && <p>updating...</p>}
 									</div>
-									<div className={scss.user_contacts_block}>
+
+									{isUpdating && <p>updating...</p>}
+									<div className={scss.email}>
+										<span>Email address</span>
 										<p>
-											<IconMail stroke={2} /> {tempEmail}
+											<IconMail size={20} color='#7d7d7d' /> {tempEmail}
 										</p>
+									</div>
+									<div className={scss.left__personal_info__number}>
 										{tempPhone && (
 											<p>
 												<IconPhone stroke={2} /> {tempPhone}
@@ -260,7 +286,7 @@ const Profile: FC = () => {
 										)}
 									</div>
 								</div>
-								<div className={scss.right_right}>
+								<div className={scss.right_tg}>
 									<div className={scss.telegram}>
 										{dataCheckTelegramAuth?.results !== null ? (
 											<>
@@ -269,22 +295,6 @@ const Profile: FC = () => {
 													<></>
 												) : (
 													<div className={scss.profile}>
-														{/* <h2 className={scss.firstName_lastName}>
-															{dataCheckTelegramAuth?.results.firstName}{' '}
-															{dataCheckTelegramAuth?.results.lastName}
-														</h2>
-														<h3 className={scss.username}>
-															{dataCheckTelegramAuth?.results.username}
-														</h3>
-														<Button
-															className={scss.button}
-															variant="outline"
-															size="xs"
-															onClick={handleDisconnectTelegramAuth}
-															leftSection={<IconMobiledataOff size={18} />}
-														>
-															Отвязать Telegram
-														</Button> */}
 														<button
 															className={scss.button}
 															onClick={() => setIsOpenTelegramModal(true)}
@@ -301,6 +311,71 @@ const Profile: FC = () => {
 										)}
 									</div>
 								</div>
+							</div>
+						</div>
+						<div className={scss.right}>
+							<div className={scss.bio_description}>
+								<span>Bio</span>
+								<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cum rerum debitis quod, officiis voluptatem corporis repudiandae .
+									fdfdfdfsfdsfsfdffsfdsfdsf
+
+								</div>
+							</div>
+							<div className={scss.interests_hobby}>
+								<div>
+									<span>Interests/Skills</span>
+									<div className={scss.ticket_cover}>
+										<div className={scss.ticket}>
+											UI Design <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											UI Design <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											UI Design <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											Framer <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											Crypto <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											ML learning <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											Fullstack <RxCross2 />
+										</div>
+										<div className={scss.ticket}>
+											UI Design <RxCross2 />
+										</div>
+									</div>
+								</div>
+								<button>+ Add more</button>
+							</div>
+							<div className={scss.social_media_acc}>
+								<span>Social Media accounts</span>
+								<div className={scss.social_cover}>
+									<div className={scss.social_item}>
+										<BsTwitterX size={22} />
+										<span>
+											https://twitter.com/Shalt0ni
+										</span>
+									</div>
+									<div className={scss.social_item}>
+										<FaInstagram size={22} />
+										<span>
+											https://instagram.com/Shalt0ni
+										</span>
+									</div>
+									<div className={scss.social_item}>
+										<FaLinkedinIn size={22} />
+										<span>
+											https://wwww.linkedin.com/Shalt0ni
+										</span>
+									</div>
+								</div>
+								<button>+ Add more</button>
 							</div>
 						</div>
 					</div>
@@ -338,7 +413,6 @@ const Profile: FC = () => {
 							<div className={scss.buttons}>
 								<Button
 									className={scss.button}
-									// variant="outline"
 									size="xs"
 									onClick={handleDisconnectTelegramAuth}
 								>
@@ -362,3 +436,20 @@ const Profile: FC = () => {
 };
 
 export default Profile;
+
+{/* <h2 className={scss.firstName_lastName}>
+					{dataCheckTelegramAuth?.results.firstName}{' '}
+					{dataCheckTelegramAuth?.results.lastName}
+				</h2>
+				<h3 className={scss.username}>
+					{dataCheckTelegramAuth?.results.username}
+				</h3>
+				<Button
+					className={scss.button}
+					variant="outline"
+					size="xs"
+					onClick={handleDisconnectTelegramAuth}
+					leftSection={<IconMobiledataOff size={18} />}
+				>
+					Отвязать Telegram
+				</Button> */}
